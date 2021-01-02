07d7eb37d0a521456499337b112c7e4c9593e226
mario <148373644@qq.com>
2021-01-02 22:30:21 +0800

    fix：最小覆盖子串更优解

--- LeetCode_JS/数组/最小覆盖子串.js
+++ LeetCode_JS/数组/最小覆盖子串.js
@@ -11,49 +11,97 @@
  * 滑动窗口算法
  * @param {*} s
  * @param {*} t
+ * ***************************
+ * 思路：1.双指针做一个滑动窗口
+ * 2.移动右指针 找到包含t的子串 移动左指针，
+ * 3 尽量减少包含t的子串长度
  */
 
-let s = 'aa',
-  t = 'a';
-var minWindow = function (s, t) {
+let s = 'ADOBECODEBANC',
+  t = 'ABC';
+var minWindow1 = function (s, t) {
   let hash = {};
   [...t].forEach((el) => (hash[el] ? hash[el]++ : (hash[el] = 1)));
   console.log('arr :>> ', hash);
 
-  let c = 0,
-    res,
-    minL = Infinity,
+  let minL = Infinity,
     start = 0,
-    count = t.length,
+    count = t.length, //类型数量
     l = 0,
-    r = 0;
+    r = 0,
+    res = '';
 
   while (r < s.length) {
+    //判断如果hash对象中有这个字符就-1：表示找到了一个字符
     if (hash[s[r]] > 0) {
       hash[s[r]]--;
     }
+    //如果某一个字符找到了 类型数量也要相应 -1 表示不需要它了
     if (hash[s[r]] === 0) {
       count--;
     }
 
-    //说明我找齐了几个字母 1.计算长度  2.试试收缩窗口
+    //说明我找齐了几个字母 开始移动左指针 1.计算长度  2.试试收缩窗口
     while (count === 0) {
-      if (r - l + 1 < minL) {
-        minL = r - l + 1;
-        start = l;
+      console.log('s.substring :>> ', s.substring(l, r + 1));
+      const result = s.substring(l, r + 1);
+      //最小长度
+      if (!res || result.length < res.length) {
+        res = result;
       }
-      //我缩小窗口丢弃了一个元素 但这个元素存在 则要+1
-      if (hash[s[l]] >= 0) {
+      if (hash[s[l]] === 0) {
+        //移动左指针
         hash[s[l]]++;
       }
-      if (hash[s[l]] > 0) {
+      //如果移动到的字符 等于1 那就表示需要这个字符  类型数量相应+1
+      if (hash[s[l]] === 1) {
         count++;
       }
       l++;
     }
     r++;
   }
-  return minL === Infinity ? '' : s.substring(start, start + minL);
+  return res;
+};
+
+var minWindow = function (s, t) {
+  //设置左右指针
+  let l = 0,
+    r = 0,
+    res = '';
+  let start = -1;
+  let minLen = s.length + 1;
+  const need = new Map();
+  for (let c of t) {
+    need.set(c, need.has(c) ? need.get(c) + 1 : 1);
+  }
+  console.log('need :>> ', need);
+  //设置类型数量
+  let needType = need.size;
+  //设置边界
+  while (r < s.length) {
+    const c = s[r];
+    if (need.has(c)) {
+      need.set(c, need.get(c) - 1);
+      if (need.get(c) === 0) needType--;
+    }
+    while (needType === 0) {
+      // 滑动窗口宽度如果比minLen小，就更新minLen
+      if (r - l + 1 < minLen) {
+        minLen = r - l + 1;
+        start = l; // 更新最小窗口的起点
+      }
+      const c2 = s[l];
+      if (need.has(c2)) {
+        need.set(c2, need.get(c2) + 1);
+        if (need.get(c2) === 1) needType++;
+      }
+      l++;
+    }
+    r++;
+  }
+  if (start == -1) return '';
+  return s.substring(start, start + minLen);
 };
 
 console.log(
