fadc16f35263054d212c212a50d70ae2644a342b
mario <148373644@qq.com>
2021-02-03 23:34:06 +0800

    🍻 还未完成

---
+++ LeetCode_JS/图/太平洋大西洋水流问题.js
@@ -0,0 +1,28 @@
+/**
+ * 417. 太平洋大西洋水流问题
+ * 给定下面的 5x5 矩阵:
+
+  太平洋 ~   ~   ~   ~   ~ 
+       ~  1   2   2   3  (5) *
+       ~  3   2   3  (4) (4) *
+       ~  2   4  (5)  3   1  *
+       ~ (6) (7)  1   4   5  *
+       ~ (5)  1   1   2   4  *
+          *   *   *   *   * 大西洋
+
+返回:
+
+[[0, 4], [1, 3], [1, 4], [2, 2], [3, 0], [3, 1], [4, 0]] (上图中带括号的单元).
+
+ */
+
+var pacificAtlantic = function (matrix) {
+  //边界条件
+  if (!matrix || matrix[0]) return [];
+
+  // 创建2个矩阵
+  const m = matrix.length;
+  const n = matrix[0].length;
+  const flow1 = Array.from({ length: m }, () => new Array(n).fill(false));
+  const flow2 = Array.from({ length: m }, () => new Array(n).fill(false));
+};
