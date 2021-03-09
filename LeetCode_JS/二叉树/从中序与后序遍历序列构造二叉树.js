4de30edcd716f72ad98764e1f54435a9c51ff13f
mario <148373644@qq.com>
2021-03-09 22:04:16 +0800

    📖 从中序与后序遍历序列构造二叉树

--- LeetCode_JS/二叉树/从中序与后序遍历序列构造二叉树.js
+++ LeetCode_JS/二叉树/从中序与后序遍历序列构造二叉树.js
@@ -13,24 +13,29 @@
    15   7
 1.选择后序遍历的最后一个节点作为根节点。
 2.根据根节点在中序遍历中的下标判断 中序数组中根节点左右代表左子树右子树
+3.
  */
 var buildTree = function (inorder, postorder) {
   let post_idx;
   const idx_map = new Map();
 
   const helper = (in_left, in_right) => {
+     //判断节点为空
     if (in_left > in_right) return null;
-
+    
+    //寻找根节点 后序遍历的最后一个元素
     const root_val = postorder[post_idx];
-
+    
+    //创建根节点树  
     const root = new TreeNode(root_val);
 
+    // 拿到根节点在中序遍历中的下标位置
     const index = idx_map.get(root_val);
-
+    //下一个子树中 根节点就是下标的前一个  [[3,15,20],3] 那就是20
     post_idx--;
-    //创建右节点 左节点
+    //创建右子树  [根节点下标+1....数组长度-1]
     root.right = helper(index + 1, in_right);
-
+    //创建左子树   [初始位置0 .... 根节点位置-1]
     root.left = helper(in_left, index - 1);
 
     return root;
@@ -46,7 +51,7 @@ var buildTree = function (inorder, postorder) {
   });
 
   return helper(0, inorder.length - 1);
-  //回家想想
+  
 };
 
 function TreeNode(val, left, right) {
