a1009228b34cfcfb8d0da79b36ce8134740e3bcb
mario <148373644@qq.com>
2021-03-20 23:46:57 +0800

    二叉搜索树中的插入操作

---
+++ LeetCode_JS/二叉树/二叉搜索树中的插入操作.js
@@ -0,0 +1,29 @@
+/**
+ * 
+ * 二叉搜索树中的插入操作
+ * 给定二叉搜索树（BST）的根节点和要插入树中的值，、
+ * 将值插入二叉搜索树。 返回插入后二叉搜索树的根节点。 输入数据 保证 ，新值和原始二叉搜索树中的任意节点值都不同。
+ */
+
+
+//递归
+// 如果该子树不为空，则问题转化成了将val 插入到对应子树上。
+// 否则，在此处新建一个以val 为值的节点，并链接到其父节点root 上。
+
+
+var insertIntoBST = function(root, val) {
+   if(root === null) {
+     return new TreeNode(val)
+   }
+
+   if(root.val > val){
+      root.left = insertIntoBST(root.left,val)
+   }
+
+   if(root.val < val){
+     root.right = insertIntoBST(root.right,val)
+   }
+
+   return root
+    
+};
